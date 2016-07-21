f74c8147d64d470b6719243764a7bb04e5e29f30
kendallgoodwin <kendall@kendallgoodwin.com>
2016-07-21 16:57:00 -0700

    angular working

--- app/controllers.js
+++ app/controllers.js
@@ -1,1 +1,102 @@
 angular.module('WeddingCtrl', ['ngAnimate', 'ui.bootstrap', 'bootstrapLightbox'])
+
+app.controller('GalleryCtrl', function ($scope, Lightbox) {
+$scope.name = "Kendall Maniulit"
+
+$scope.images = [
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+    {
+      'url': 'images/.jpg',
+    },
+  ];
+
+  $scope.openLightboxModal = function (index) {
+    Lightbox.openModal($scope.images, index);
+  };
+});
